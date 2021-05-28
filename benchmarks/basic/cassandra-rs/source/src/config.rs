85e124a9f05f5e3a206bf5c629ffb9fc4b726903
cvybhu <cvybhu@selade.com>
2021-05-28 20:04:33 +0200

    Add cassandra-rs

---
+++ benchmarks/basic/cassandra-rs/source/src/config.rs
@@ -0,0 +1,100 @@
+use anyhow::{anyhow, Result};
+use getopts::Options;
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Workload {
+    Inserts,
+    Selects,
+    Mixed,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub node_address: String,
+    pub workload: Workload,
+    pub tasks: i64,
+    pub concurrency: i64,
+    pub batch_size: i64,
+    pub no_prepare: bool,
+}
+
+impl Config {
+    pub fn read() -> Result<Option<Config>> {
+        let mut opts = Options::new();
+
+        opts.optflag("h", "help", "Print usage information");
+
+        opts.optopt(
+            "a",
+            "address",
+            "Address of database node to connect to
+            (default: 'scylla')",
+            "ADDRESS",
+        );
+        opts.optopt(
+            "w",
+            "workload",
+            "Type of work to perform (inserts, selects, mixed) (default: mixed)",
+            "WORKLOAD",
+        );
+        opts.optopt("t", "tasks", "Total number of tasks (requests) to perform the during benchmark
+                                In case of mixed workload there will be tasks inserts and tasks selects
+                                (default: 1 000 000)", "TASKS");
+        opts.optopt(
+            "c",
+            "concurrency",
+            "Maximum number of requests performed at once
+            (default: 1024)",
+            "CONCURRENCY",
+        );
+
+        opts.optflag(
+            "n",
+            "no-prepare",
+            "Don't create tables and insert into them before the benchmark",
+        );
+
+        let args: Vec<String> = std::env::args().collect();
+        let parsed = opts.parse(&args[1..])?;
+
+        if parsed.opt_present("help") {
+            println!("{}", opts.usage("Benchmark configuration"));
+            return Ok(None);
+        }
+
+        let address: String = parsed.opt_get_default("address", "scylla".to_string())?;
+
+        let workload_str: String = parsed.opt_get_default("workload", "inserts".to_string())?;
+        let workload: Workload = match workload_str.as_str() {
+            "inserts" => Workload::Inserts,
+            "selects" => Workload::Selects,
+            "mixed" => Workload::Mixed,
+            other => {
+                return Err(anyhow!(
+                    "Invalid workload type: '{}'. Allowed values: inserts, selects, mixed",
+                    other
+                ))
+            }
+        };
+
+        let tasks: i64 = parsed.opt_get_default("tasks", 1_000_000)?;
+        let concurrency: i64 = parsed.opt_get_default("concurrency", 1024)?;
+
+        let no_prepare: bool = parsed.opt_present("no-prepare");
+
+        let mut batch_size = 256;
+
+        if tasks / batch_size < concurrency {
+            batch_size = std::cmp::max(1, tasks / concurrency);
+        }
+
+        Ok(Some(Config {
+            node_address: address,
+            workload,
+            tasks,
+            concurrency,
+            batch_size,
+            no_prepare,
+        }))
+    }
+}
---
+++ benchmarks/basic/cassandra-rs/source/src/main.rs
@@ -0,0 +1,178 @@
+mod config;
+
+use anyhow::Result;
+use cassandra_cpp::{Cluster, PreparedStatement, Session, Statement};
+use config::{Config, Workload};
+use std::convert::TryInto;
+use std::sync::atomic::{AtomicI64, Ordering};
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("Starting cassandra-rs benchmark\n");
+
+    let config: Arc<Config> = match Config::read()? {
+        Some(config) => Arc::new(config),
+        None => return Ok(()), // --help only prints usage
+    };
+
+    println!("Benchmark configuration:\n{:#?}\n", config);
+
+    let mut cluster = Cluster::default();
+    cluster.set_contact_points(&config.node_address).unwrap();
+    cluster.set_load_balance_round_robin();
+    cluster.set_queue_size_io((2 * config.concurrency).try_into().unwrap()).unwrap();
+    let session: Arc<Session> = Arc::new(cluster.connect_async().await.unwrap());
+
+    if !config.no_prepare {
+        prepare_keyspace_and_table(&session).await?;
+    }
+
+    let insert_stmt = "INSERT INTO benchks.benchtab (pk, v1, v2) VALUES(?, ?, ?)";
+    let select_stmt = "SELECT v1, v2 FROM benchks.benchtab WHERE pk = ?";
+
+    let prepared_insert = Arc::new(session.prepare(insert_stmt).unwrap().await.unwrap());
+    let prepared_select = Arc::new(session.prepare(select_stmt).unwrap().await.unwrap());
+
+    if config.workload == Workload::Selects && !config.no_prepare {
+        prepare_selects_benchmark(&session, &prepared_insert, &config).await?;
+    }
+
+    let mut handles = Vec::with_capacity(config.concurrency.try_into().unwrap());
+    let next_batch_start = Arc::new(AtomicI64::new(0));
+
+    println!("\nStarting the benchmark");
+
+    let start_time = std::time::Instant::now();
+
+    for _ in 0..config.concurrency {
+        let session = session.clone();
+        let prepared_insert = prepared_insert.clone();
+        let prepared_select = prepared_select.clone();
+        let config = config.clone();
+        let next_batch_start = next_batch_start.clone();
+
+        handles.push(tokio::spawn(async move {
+            loop {
+                let cur_batch_start: i64 =
+                    next_batch_start.fetch_add(config.batch_size, Ordering::Relaxed);
+
+                if cur_batch_start >= config.tasks {
+                    // No more work to do
+                    break;
+                }
+
+                let cur_batch_end: i64 =
+                    std::cmp::min(cur_batch_start + config.batch_size, config.tasks);
+
+                for pk in cur_batch_start..cur_batch_end {
+                    if config.workload == Workload::Inserts || config.workload == Workload::Mixed {
+                        let mut insert_stmt = prepared_insert.bind();
+                        insert_stmt.bind_int64(0, pk).unwrap();
+                        insert_stmt.bind_int64(1, 2 * pk).unwrap();
+                        insert_stmt.bind_int64(2, 3 * pk).unwrap();
+
+                        let fut = session.execute(&insert_stmt);
+                        fut.await.unwrap();
+                    }
+
+                    if config.workload == Workload::Selects || config.workload == Workload::Mixed {
+                        let mut select_stmt = prepared_select.bind();
+                        select_stmt.bind_int64(0, 2 * pk).unwrap();
+                        select_stmt.bind_int64(1, 3 * pk).unwrap();
+
+                        let fut = session.execute(&select_stmt);
+                        let res = fut.await.unwrap();
+                        let first_row = res.first_row().unwrap();
+
+                        let v1: i64 = first_row.get_column(0).unwrap().get_i64().unwrap();
+                        let v2: i64 = first_row.get_column(1).unwrap().get_i64().unwrap();
+
+                        assert_eq!((v1, v2), (2 * pk, 3 * pk));
+                    }
+                }
+            }
+        }));
+    }
+
+    for handle in handles {
+        handle.await?;
+    }
+
+    let bench_time = start_time.elapsed();
+    println!("Finished\n\nBenchmark time: {} ms", bench_time.as_millis());
+
+    Ok(())
+}
+
+async fn prepare_keyspace_and_table(session: &Session) -> Result<()> {
+    let drop_ks = Statement::new("DROP KEYSPACE IF EXISTS benchks", 0);
+
+    session.execute(&drop_ks).await.unwrap();
+
+    let create_ks = Statement::new(
+        "CREATE KEYSPACE IF NOT EXISTS benchks WITH REPLICATION = \
+    {'class' : 'SimpleStrategy', 'replication_factor' : 1}",
+        0,
+    );
+
+    session.execute(&create_ks).await.unwrap();
+
+    let create_table = Statement::new(
+        "CREATE TABLE IF NOT EXISTS benchks.benchtab (pk bigint PRIMARY KEY, v1 bigint, v2 bigint)",
+        0,
+    );
+
+    session.execute(&create_table).await.unwrap();
+
+    Ok(())
+}
+
+async fn prepare_selects_benchmark(
+    session: &Arc<Session>,
+    prepared_insert: &Arc<PreparedStatement>,
+    config: &Arc<Config>,
+) -> Result<()> {
+    println!("Preparing a selects benchmark (inserting values)...");
+
+    let mut handles = Vec::with_capacity(config.concurrency.try_into().unwrap());
+    let next_batch_start = Arc::new(AtomicI64::new(0));
+
+    for _ in 0..config.concurrency {
+        let session = session.clone();
+        let prepared_insert = prepared_insert.clone();
+        let config = config.clone();
+        let next_batch_start = next_batch_start.clone();
+
+        handles.push(tokio::spawn(async move {
+            loop {
+                let cur_batch_start: i64 =
+                    next_batch_start.fetch_add(config.batch_size, Ordering::Relaxed);
+
+                if cur_batch_start >= config.tasks {
+                    // No more work to do
+                    break;
+                }
+
+                let cur_batch_end: i64 =
+                    std::cmp::min(cur_batch_start + config.batch_size, config.tasks);
+
+                for pk in cur_batch_start..cur_batch_end {
+                    let mut insert_stmt = prepared_insert.bind();
+                    insert_stmt.bind_int64(0, pk).unwrap();
+                    insert_stmt.bind_int64(1, 2 * pk).unwrap();
+                    insert_stmt.bind_int64(2, 3 * pk).unwrap();
+
+                    let fut = session.execute(&insert_stmt);
+                    fut.await.unwrap();
+                }
+            }
+        }));
+    }
+
+    for handle in handles {
+        handle.await?;
+    }
+
+    Ok(())
+}
