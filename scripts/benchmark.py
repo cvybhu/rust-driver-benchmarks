d35af13898b629a82544afb68d5a7965b270b553
cvybhu <cvybhu@selade.com>
2021-05-29 03:47:19 +0200

    Add benchmarking scripts

---
+++ scripts/benchmark.py
@@ -0,0 +1,54 @@
+import subprocess
+
+task_options = [1000 * 1000]
+concurrency_options = [64, 128, 256, 512, 1024, 2048, 4096, 8192]
+workload_options = ["inserts", "selects", "mixed"]
+
+drivers = ["rust", "cpp", "cassandra-rs", "gocql", "cdrs-tokio"]
+
+# Each configuration is repeated samples times to ensure consistent results
+samples = 3
+
+nodes_no_ports = ["scylla"]
+#nodes_no_ports = ["10.0.0.226", "10.0.1.52", "10.0.2.86"]
+
+nodes_with_ports = [n + ":9042" for n in nodes_no_ports]
+
+configurations = []
+for t in task_options:
+    for c in concurrency_options:
+        for w in workload_options:
+            configurations.append((t, c, w))
+
+# Returns estimated time in seconds to complete single sample of a benchmark using rust driver
+def estimate_time(configuration):
+    t = configuration[0] / configuration[1]
+
+    # Scale so that 1000 000 inserts with concurrency 64 takes 250 seconds to match scylla-rust-driver
+    t = t * 250/15625 
+
+    # Take into account the time needed to prepare a selects benchmark
+    if configuration[2] == "selects" or configuration[2] == "mixed":
+        t += estimate_time((configuration[0], max(1024, configuration[1]), "inserts"))
+
+    return t
+
+sorted_confs = sorted(configurations, key = lambda c : estimate_time(c))
+
+for (tasks, concurrency, workload) in sorted_confs:
+    for driver in drivers:
+        for sample in range(samples):
+            # Run the benchmark
+            nodes = ""
+            if driver in ["cpp", "cassandra-rs"]:
+                nodes = ",".join(nodes_no_ports)
+            else:
+                nodes = ",".join(nodes_with_ports)
+
+            print(f'\nBENCHMARK {{"driver": "{driver}", "workload": "{workload}", "concurency": {concurrency}, "tasks": {tasks}, "sample": {sample}}}')
+
+            subprocess.call([f"../benchmarks/basic/{driver}/run.sh", 
+                             "--address", nodes, 
+                             "--workload", workload,
+                             "--concurrency", str(concurrency),
+                             "--tasks", str(tasks)])
---
+++ scripts/estimate_bench_time.py
@@ -0,0 +1,52 @@
+# This script helps estimate how long a series of benchmarks would take
+
+import os
+
+task_options = [1000 * 1000]
+concurrency_options = [64, 128, 256, 512, 1024, 2048, 4096, 8192]
+workflow_options = ["inserts", "selects", "mixed"]
+
+drivers = ["rust", "cpp", "cassandra-rs", "cdrs-tokio", "gocql"]
+
+restarts = False
+
+# Each configuration is repeated samples times to ensure consistent results
+samples = 3
+
+configurations = []
+for t in task_options:
+    for c in concurrency_options:
+        for w in workflow_options:
+            configurations.append((t, c, w))
+
+# Returns estimated time in seconds to complete single sample of a benchmark using rust driver
+def estimate_time(configuration):
+    t = configuration[0] / configuration[1]
+
+    # Scale so that 1000 000 inserts with concurrency 64 takes 250 seconds to match scylla-rust-driver
+    t = t * 250/15625 
+
+    # Take into account the time needed to prepare a selects benchmark
+    if configuration[2] == "selects" or configuration[2] == "mixed":
+        t += estimate_time((configuration[0], max(1024, configuration[1]), "inserts"))
+
+    return t
+
+# How much time each driver takes compared to scylla-rust-driver
+rust_coef = 1
+cpp_coef = 5510/3734
+cdrs_tokio_coef = 17840/3734
+gocql_coef = 14802/3734
+cassandra_rs_coef = cpp_coef
+
+total_coef = rust_coef + cpp_coef + cdrs_tokio_coef + gocql_coef + cassandra_rs_coef
+
+
+total_time = sum([estimate_time(c) * total_coef * samples for c in configurations])
+
+if restarts:
+    # For each configuration and sample each driver does a 4 minute restart
+    total_time += len(configurations) * samples * 5 * 4 * 60
+
+print(f"Number of configurations: {len(configurations)}")
+print(f"total_time: {total_time / 60 / 60} hours")
